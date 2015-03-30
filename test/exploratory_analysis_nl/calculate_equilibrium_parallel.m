d2e6e6b902d34f9453937ff38a459994712a999c
rafaelmourao <rafael@mourao.org>
2015-03-30 12:51:50 +0200

    Updated calculation and printing script for the new tests

--- test/exploratory_analysis_nl/calculate_equilibrium_parallel.m
+++ test/exploratory_analysis_nl/calculate_equilibrium_parallel.m
@@ -7,74 +7,98 @@ Date:   2015-03-18
 %}
 
 clear;
-addpath('../classes')
+addpath('../../classes')
 load('Populations.mat')
 rng(1);
 
-test.contracts = 1:Population(1).nContracts;
+test(1).contracts = 1:Population(1).nContracts;
+test(2).contracts = 1:Population(1).nContracts;
+test(3).contracts = 1:Population(1).nContracts;
+test(4).contracts = 1:Population(1).nContracts;
 
-costOfPublicFunds = 0;
+test(1).populations = 1:6;
+test(2).populations = 7:12;
+test(3).populations = 13:18;
+test(4).populations = 19:21;
+
+[test.mandate] = deal(7); % index of the first mandate contract
+
+[test.costOfPublicFunds] = deal(0);
+
 CalculationParametersEquilibrium.behavioralAgents = 0.01;
-CalculationParametersEquilibrium.fudge            = 5e-5;
-CalculationParametersEquilibrium.maxIterations    = 1e6;
+CalculationParametersEquilibrium.fudge            = 1e-6;
+CalculationParametersEquilibrium.maxIterations    = 1e4;
 CalculationParametersEquilibrium.tolerance        = 1;
+CalculationParametersEquilibrium.lineSearchErrorTolerance = Inf;
 display(CalculationParametersEquilibrium)
+
 CalculationParametersOptimum.maxIterations        = 1e3;
 CalculationParametersOptimum.tolerance            = 0.01;
+CalculationParametersOptimum.knitro               = 'true';
 display(CalculationParametersOptimum)
 
-nPopulations = length(Population);
-nworkers = 2*length(Population) * length(test);
+[test.CalculationParametersEquilibrium] = deal(CalculationParametersEquilibrium);
+[test.CalculationParametersOptimum] = deal(CalculationParametersOptimum);
 
 Population_old = Population;
 iter = 0;
 
-for i = 1:length(test) 
+for i = 1:length(test)
+
+    nPopulations = length(test(i).populations);
     
-test(i).costOfPublicFunds = costOfPublicFunds;
-test(i).CalculationParametersEquilibrium = CalculationParametersEquilibrium;
-test(i).CalculationParametersOptimum = CalculationParametersOptimum;
 for j = 1:nPopulations
     iter = iter + 1;
-    Population(iter) = Population_old(j);
+    CalculationParametersEquilibrium(i) = test(i).CalculationParametersEquilibrium;
+    CalculationParametersOptimum(i) = test(i).CalculationParametersOptimum;
+    costOfPublicFunds(i) = test(i).costOfPublicFunds;    
+    Population(iter) = Population_old(test(i).populations(j));
     Population(iter).uMatrix = Population(iter).uMatrix(:,test(i).contracts);
     Population(iter).cMatrix = Population(iter).cMatrix(:,test(i).contracts);
     Population(iter).nContracts = length(test(i).contracts);
-    test(i).Model(j) = Model(j);
+    test(i).Model(j) = Model(test(i).populations(j));
     test(i).Model(j).contracts = test(i).Model(j).contracts(:,test(i).contracts);
 end
 for j = (nPopulations+1):(2*nPopulations)
     iter = iter + 1;
+    CalculationParametersEquilibrium(i) = test(i).CalculationParametersEquilibrium;
+    CalculationParametersOptimum(i) = test(i).CalculationParametersOptimum;
+    costOfPublicFunds(i) = test(i).costOfPublicFunds;    
     Population(iter) = Population(iter-nPopulations);
-    Population(iter).uMatrix = Population(iter-nPopulations).uMatrix(:,2:end);
-    Population(iter).cMatrix = Population(iter-nPopulations).cMatrix(:,2:end);
-    Population(iter).nContracts = length(test(i).contracts)-1;
+    Population(iter).uMatrix = Population(iter-nPopulations).uMatrix(:,test(i).mandate:end);
+    Population(iter).cMatrix = Population(iter-nPopulations).cMatrix(:,test(i).mandate:end);
+    Population(iter).nContracts = length(test(i).contracts)-(test(i).mandate-1);
     test(i).Model(j) = test(i).Model(j-nPopulations);
-    test(i).Model(j).contracts = test(i).Model(j).contracts(:,2:end);
+    test(i).Model(j).contracts = test(i).Model(j).contracts(:,test(i).mandate:end);
 end
 
+test(i).nPopulations = nPopulations;
+
 end
+
+nworkers = 2 * sum(cat(1,test.nPopulations));
 
 if ~isempty(gcp('nocreate'))
      delete(gcp)
 end
+
 poolobj = parpool(nworkers)
 
 parfor i = 1:nworkers
     
 [pEquilibrium{i}, DEquilibrium{i}, ACEquilibrium{i}, ComputationOutputEquilibrium{i}] = ...
-         Population(i).findequilibrium(CalculationParametersEquilibrium)
-WEquilibrium{i} = Population(i).welfare(pEquilibrium{i}, costOfPublicFunds)
+         Population(i).findequilibrium(CalculationParametersEquilibrium(i))
+WEquilibrium{i} = Population(i).welfare(pEquilibrium{i}, costOfPublicFunds(i))
 
 [pEfficient{i}, WEfficient{i}, ComputationOutputEfficient{i}] = ...
-            findefficient(Population(i), costOfPublicFunds, CalculationParametersOptimum, pEquilibrium{i})
+            findefficient(Population(i), costOfPublicFunds(i), CalculationParametersOptimum(i), pEquilibrium{i})
         
 DEfficient{i} = Population(i).demand(pEfficient{i})
 
 end
 
 for i = 1:length(test)
-    for j = 1:(2*nPopulations)
+    for j = 1:test(i).nPopulations
     z = (2*nPopulations)*(i-1) + j;
     test(i).pEquilibrium{j} = pEquilibrium{z};
     test(i).DEquilibrium{j} = DEquilibrium{z};
--- test/exploratory_analysis_nl/print_lognormal_tables.m
+++ test/exploratory_analysis_nl/print_lognormal_tables.m
@@ -10,19 +10,16 @@ print to an text files
 
 %}
 
-addpath('../classes')
+addpath('../../classes')
 load('tests.mat')
 format shortg
 
 file = @(x) ['test_lognormal_' num2str(x) '.txt'];
-Testnames = {'High Aversion, Low MH Variance', 'Medium Aversion, Low MH Variance',...
-    'Low Aversion, Low MH Variance', 'High Aversion, High MH Variance',...
-    'Medium Aversion, High MH Variance', 'Low Aversion, High MH Variance'};
 
 for i = 1:length(test)
    
 ncontracts = length(test(i).contracts);
-nPopulations = length(test(i).Model) / 2; % Number of population objects is doubled to consider the mandate case
+nPopulations = test(i).nPopulations;
 contractinfo = {'Deductible','Coinsurance','OOP Max','Mean Coverage'}';
 for j = 1:ncontracts
     contractinfo{1,j+1} = test(i).Model(1).contracts{j}.deductible;
@@ -35,28 +32,27 @@ fid = fopen(file(i),'w+');
 string = evalc('disp(contractinfo)');
 fwrite(fid,string);
 
+fprintf(fid,'\nEquilibrium Calculation Parameters\n\n');
+fwrite(fid,evalc('disp(CalculationParametersEquilibrium)'));
+fprintf(fid,'\nOptimum Calculation Parameters\n\n');
+fwrite(fid,evalc('disp(CalculationParametersOptimum)'));
+
 x = cell(8,ncontracts);
 Tables = cell(1,ncontracts);
- 
+
+
 for j = 1:nPopulations
     x(1,:)=num2cell(round(test(i).DEquilibrium{j},2));
     x(2,:)=num2cell(test(i).pEquilibrium{j});
     x(3,1)=num2cell(test(i).WEquilibrium{j});
-    x(4,1)=num2cell(test(i).ComputationOutputEquilibrium{j}.error);
-    x(5,:)=num2cell(round(test(i).DEfficient{j},2));
-    x(6,:)=num2cell(test(i).pEfficient{j});
-    x(7,1)=num2cell(test(i).WEfficient{j});
-    x(8,1)=num2cell(test(i).ComputationOutputEfficient{j}.error);
-    x(9,2:end)=num2cell(round(test(i).DEquilibrium{j+nPopulations},2));
-    x(10,2:end)=num2cell(test(i).pEquilibrium{j+nPopulations});
-
-    rowtitles = {'Equilibrium Demand','Equilibrium Prices',...
-          'Equilibrium Welfare', 'Equilibrium Error', 'Efficient Demand','Efficient Prices',...
-          'Efficient Welfare','Efficient Error', 'Mandate Demand','Mandate Prices'}';
-
+    x(4,1)=num2cell(test(i).ComputationOutputEquilibrium{j}.error);   
     string = evalc('disp([rowtitles,x])');
-
-    fprintf(fid,['\n' Testnames{j} '\n\n']);
+    
+%     fprintf(fid,['\n\n' Testnames{j} '\n\n\n']);
+    fprintf(fid,'\n\nMean Parameters\n\n');
+    fwrite(fid,evalc('disp(num2cell(Model(j).typeDistributionMean))'));
+    fprintf(fid,'\nLog Covariances\n\n');
+    fwrite(fid,evalc('disp(num2cell(Model(j).typeDistributionLogCovariance))'));
     fwrite(fid,string);
 end
 
