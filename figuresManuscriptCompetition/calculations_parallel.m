02f5edc93f3dd5938794125b84b44964530cfe75
Eduardo Azevedo <eduaze@gmail.com>
2016-03-29 13:40:40 -0400

    Added tougher mandate calculation for comparative statics section.

--- figuresManuscriptCompetition/calculations_parallel.m
+++ figuresManuscriptCompetition/calculations_parallel.m
@@ -11,7 +11,7 @@ typeDistributionLogCovariance = ...
      -0.01  0.28 -0.03 0    ; % c22 = 0.98 originally
      -0.12 -0.03  0.20 0    ; % c33 = 0.20 originally
       0     0     0    0.25]; % ???
-  
+
 costOfPublicFunds = 0;
 
 % Calculation parameters
@@ -37,13 +37,17 @@ modelName{2}              = 'mandate';
 slopeVector{2}            = 0.60:0.04:1;
 moralHazardLogVariance{2} = 0.28;
 
-modelName{3}              = 'interval_high_mh_variance';
-slopeVector{3}            = 0:0.04:1;
-moralHazardLogVariance{3} = 0.98;
+modelName{3}              = 'mandate_tougher';
+slopeVector{3}            = 0.60:0.04:1;
+moralHazardLogVariance{3} = 0.28;
 
-modelName{4}              = 'mandate_high_mh_variance';
-slopeVector{4}            = 0.60:0.04:1;
+modelName{4}              = 'interval_high_mh_variance';
+slopeVector{4}            = 0:0.04:1;
 moralHazardLogVariance{4} = 0.98;
+
+modelName{5}              = 'mandate_high_mh_variance';
+slopeVector{5}            = 0.60:0.04:1;
+moralHazardLogVariance{5} = 0.98;
 
 % Loop
 nSimulations = length(modelName);
@@ -55,21 +59,21 @@ parfor i = 1 : nSimulations
 
     innerTypeDistributionLogCovariance(2, 2) = moralHazardLogVariance{i};
     Model = healthcaralognormalmodel(slopeVector{i}, typeDistributionMean, innerTypeDistributionLogCovariance);
-    
+
     Population = population(Model, populationSize);
 
     [pEquilibrium, DEquilibrium, ACEquilibrium, ComputationOutputEquilibrium] = ...
             Population.findequilibrium(CalculationParametersEquilibrium);
     WEquilibrium = Population.welfare(pEquilibrium, ...
                                           costOfPublicFunds);
-    
+
     [pEfficient, WEfficient, ComputationOutputEfficient] = ...
             findefficient(Population, costOfPublicFunds, CalculationParametersOptimum);
     DEfficient = Population.demand(pEfficient);
 
     display(modelName{i});
     display(ComputationOutputEfficient);
-    display(ComputationOutputEquilibrium);    
+    display(ComputationOutputEquilibrium);
     parsave(modelName{i}, Model, pEquilibrium, DEquilibrium, ...
                 ACEquilibrium, ComputationOutputEquilibrium, ...
             WEquilibrium, pEfficient, WEfficient, ...
