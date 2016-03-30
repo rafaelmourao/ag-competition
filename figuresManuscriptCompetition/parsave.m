1b54330f68f2738dfddd16c3b8cbe5104942f700
Eduardo Azevedo <eduaze@gmail.com>
2016-03-30 09:11:54 -0400

    Fixed bug with not using cell array of strings for parsave.

--- figuresManuscriptCompetition/parsave.m
+++ figuresManuscriptCompetition/parsave.m
@@ -2,17 +2,20 @@ function parsave(fname, varnames, varargin)
 numvars=numel(varargin);
 fname = [fname, '.mat'];
 
-if (length(varnames) ~= numvars)
+if (numel(varnames) ~= numvars)
     display(['Error! Vector of names has a different number of ' ...
-             'elements than the number of variables!'])
-
-for i=1:numvars
-       eval([varnames(i), '=varargin{i};']);  
+             'elements than the number of variables!']);
+    display(varnames);
+    display(varargin);
 end
 
-save('-mat', fname, varnames(1));
+for i=1:numvars
+       eval([varnames{i}, '=varargin{i};']);  
+end
+
+save('-mat', fname, varnames{1});
 
 for i = 2:numvars    
-    save('-mat', fname, varnames(i), '-append');
+    save('-mat', fname, varnames{i}, '-append');
 end
 end
