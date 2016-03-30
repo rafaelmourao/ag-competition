eedbf9afdadb50cdae08960421cb9ee9f310bb8b
Eduardo Azevedo <eduaze@gmail.com>
2016-03-30 08:34:09 -0400

    Fixed parsave so that it does not use inputname, which caused a bug with the newer version of MATLAB. Now parsave takes as input the variable names. It complains if the vector of variable names has the wrong length, but it does not do any more error checking on the names, so watch out for that in the future or improve it.

--- figuresManuscriptCompetition/parsave.m
+++ figuresManuscriptCompetition/parsave.m
@@ -1,11 +1,18 @@
-function parsave(fname, varargin)
+function parsave(fname, varnames, varargin)
 numvars=numel(varargin);
 fname = [fname, '.mat'];
+
+if (length(varnames) ~= numvars)
+    display(['Error! Vector of names has a different number of ' ...
+             'elements than the number of variables!'])
+
 for i=1:numvars
-       eval([inputname(i+1),'=varargin{i};']);  
-       end
-       save('-mat',fname,inputname(2));
-       for i = 2:numvars    
-           save('-mat',fname,inputname(i+1),'-append');
-           end
+       eval([varnames(i), '=varargin{i};']);  
+end
+
+save('-mat', fname, varnames(1));
+
+for i = 2:numvars    
+    save('-mat', fname, varnames(i), '-append');
+end
 end
