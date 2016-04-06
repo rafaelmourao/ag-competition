07472d5dbaa116f5d877a2780db5cf84d1706450
Eduardo Azevedo <eduaze@gmail.com>
2016-04-06 16:29:54 -0400

    Added comparative statics to bash script

--- figuresManuscriptCompetition/run_directory.sh
+++ figuresManuscriptCompetition/run_directory.sh
@@ -8,6 +8,7 @@
 export MATLAB_PARALLEL_RAM=31G
 matlab -nodisplay < calculations_parallel.m;
 matlab -nodisplay < welfare_table.m;
+matlab -nodisplay < comparative_statics.m
 matlab < plot_figures.m;
 
 ssh unix.wharton.upenn.edu 'dropbox stop; sleep 20; dropbox start; sleep 120;'
